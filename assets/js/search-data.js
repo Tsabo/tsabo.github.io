9c9fa8ff517e2099e3bc9deb565c6fd3ebb8622e
Tsabo <jeremy@browns.info>
2025-10-11 14:42:49 +0000

    Deploying to gh-pages from @ Tsabo/tsabo.github.io@d13e3da19f5470f28b8f3065145b3b11caa37bfa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,14 +11,14 @@ ninja.data = [{
     },
   },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Senior Software Engineer and Systems Architect with 23+ years of experience designing modular, observable, and high-performance infrastructure. From semantic search and AI integration to cloud-native deployments and secure access control, this page showcases the technologies, projects, and principles that drive how I build.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
